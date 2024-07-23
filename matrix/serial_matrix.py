6cb810d77dd457c1fdce4841405a52b777a4f89c
Pedro <pedro.005henr@gmail.com>
2024-07-22 22:42:33 -0300

    Serial matrix multiplication update

--- matrix/serial_matrix.py
+++ matrix/serial_matrix.py
@@ -1,9 +1,10 @@
+from numba import jit
 import numpy as np
 import time
 
-MAX_M = 10000
+MAX_M = 2048
 
-
+@jit()
 def mulmat(ma, mb, mc):
     for i in range(MAX_M):
         for j in range(MAX_M):
@@ -12,17 +13,18 @@ def mulmat(ma, mb, mc):
 
 
 def main():
-    ma = np.random.randint(10, size=(MAX_M, MAX_M))
-    mb = np.random.randint(10, size=(MAX_M, MAX_M))
-    mc = np.zeros(shape=(MAX_M, MAX_M))
+    for i in range(5):
+        ma = np.random.randint(10, size=(MAX_M, MAX_M))
+        mb = np.random.randint(10, size=(MAX_M, MAX_M))
+        mc = np.zeros(shape=(MAX_M, MAX_M))
 
-    start_time = time.time()
-    mulmat(ma, mb, mc)
-    end_time = (time.time() - start_time)
+        start_time = time.time()
+        mulmat(ma, mb, mc)
+        end_time = (time.time() - start_time)
 
-    print("[%dx%d]" % (MAX_M, MAX_M))
-    print("Elapsed time: %.4f seconds" % end_time)
-    print("%.4f\n" % end_time)
+        print("[%dx%d]" % (MAX_M, MAX_M))
+        print("Elapsed time: %.4f seconds" % end_time)
+        print("%.4f\n" % end_time)
 
 
 if __name__ == "__main__":
